22c86fd389d5b7baac8f7452e19f337cff8efc17
lunxi <446444124@qq.com>
2025-06-17 13:16:27 +0800

    feat(order): 增加订单相关功能
    - 新增用户端和管理端的订单相关接口和功能
    - 实现订单提交、支付、取消、再次下单等功能- 添加订单状态统计和搜索功能
    - 优化订单详情查询接口
    - 新增购物车批量插入接口

--- sky-server/src/main/java/com/sky/mapper/OrderDetailMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderDetailMapper.java
@@ -1,11 +1,29 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.entity.OrderDetail;
+import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 @Mapper
 public interface OrderDetailMapper {
     void insertBatch(List<OrderDetail> orderDetailList);
+
+    /**
+     * 分页条件查询并按下单时间排序
+     * @param ordersPageQueryDTO
+     */
+    Page<Orders> pageQuery(OrdersPageQueryDTO ordersPageQueryDTO);
+
+    /**
+     * 根据订单id查询订单明细
+     * @param orderId
+     * @return
+     */
+    @Select("select * from order_detail where order_id = #{orderId}")
+    List<OrderDetail> getByOrderId(Long orderId);
 }
